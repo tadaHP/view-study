9450850f4d79e670613ebabdb3cf3b685eb4d7f7
hyeonpyo <gusvy0220@gmail.com>
2024-07-11 00:50:47 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <button @click="count++">{{ count }}</button>
+  </div>
+
+  <div>
+    <span>메세지: {{ msg }}</span>
+  </div>
+
+  <div>
+    <p>v-html 디렉티브 사용: <span v-html="rawHtml"></span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      msg: 'Hello Vue!',
+      rawHtml: '<span style="color: red">빨간색 테스트</span>'
+    }
+  }
+}
+</script>
